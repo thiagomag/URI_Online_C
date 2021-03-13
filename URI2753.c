5719f3698725ce05bcc2073b138b799526da2f4e
Thiago de Oliveira Magdalena <thiagomagdalena@gmail.com>
2021-03-13 19:54:49 -0300

    Update URI2753.c

--- URI2753.c
+++ URI2753.c
@@ -7,11 +7,10 @@ int main() {
 	b = a + 1; 	c = b + 1; 	d = c + 1; 	e = d + 1; 	f = e + 1;	g = f + 1; 	h = g + 1;
 	i = h + 1;	j = i + 1;	k = j + 1;	l = k + 1;	m = l + 1;	n = m + 1;	o = n + 1;
 	p = o + 1;	q = p + 1;	r = q + 1;	s = r + 1;	t = s + 1;	u = t + 1;	v = u + 1;
-	w = v + 1;	x = 1 + w;	y = 1 + x;	z = 1 + y; 	
+	w = v + 1;	x = w + 1;	y = x + 1;	z = y + 1; 	
 	
-	printf("%d e a\n%d e b\n%d e c\n%d e d\n%d e e\n%d e f\n%d e g\n%d e h\n%d e i\n%d e j\n%d e k\n%d e l\n%d e m\n%d e n\n%d e o\n%d e p\n%d e q\n%d e r\n%d e s\n%d e t\n%d e u\n%d e v\n%d e w\n%d e x\n%d e y\n%d e z\n"
-	, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z);
- 
+	printf("%d e %c\n%d e %c\n%d e %c\n%d e %c\n%d e %c\n%d e %c\n%d e %c\n%d e %c\n%d e %c\n%d e %c\n%d e %c\n%d e %c\n%d e %c\n%d e %c\n%d e %c\n%d e %c\n%d e %c\n%d e %c\n%d e %c\n%d e %c\n%d e %c\n%d e %c\n%d e %c\n%d e %c\n%d e %c\n%d e %c\n"
+	, a, a, b, b, c, c, d, d, e, e, f, f, g, g, h, h, i, i, j, j, k, k, l, l, m, m, n, n, o, o, p, p, q, q, r, r, s, s, t, t, u, u, v, v, w, w, x, x, y, y, z, z);
  	
     return 0;
 }

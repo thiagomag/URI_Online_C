ae2fcf5bbb51bbcc3c12d1a00821911dbafba64c
Thiago de Oliveira Magdalena <thiagomagdalena@gmail.com>
2021-05-29 14:32:22 -0300

    URI Online C

--- URI1113.c
+++ URI1113.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
- 
+#include <stdbool.h>
+
+void cresc_descr(int x, int y);
+
+bool ler(int *pX, int *pY);
+
 int main() {
- 	
-	int m, n;
-		
-	while(m != n) {
-		scanf("%d", &m);
-		scanf("%d", &n);
-		if (n > m) {
-        	printf("Crescente\n");
-    	} else if (m > n){
-        	printf("Decrescente\n");
-        }
-	}
-	
+
+    int x,y;
+    while ( ler(&x, &y) )
+        cresc_descr(x,y);
     return 0;
+}
+
+bool ler(int *pX, int *pY) {
+    if(*pX != *pY) {
+        scanf("%d", &*pX);
+        scanf("%d", &*pY);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void cresc_descr(int x, int y) {
+    if (y > x) {
+        printf("Crescente\n");
+    } else if (x > y){
+        printf("Decrescente\n");
+    }
 }
---
+++ cmake-build-debug/CMakeFiles/URI_Online_C.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/URI_Online_C.dir/URI1113.c.obj"
+  "URI_Online_C.exe"
+  "URI_Online_C.exe.manifest"
+  "URI_Online_C.lib"
+  "URI_Online_C.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/URI_Online_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
